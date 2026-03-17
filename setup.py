eb6e01b0ce9b47f7191974a1bcf1b46052cf9621
Shivani Godugu <godugu06.shivani@gmail.com>
2026-03-17 12:59:22 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="ShivaniGodugu",
+    author_email="godugu.shivani@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
